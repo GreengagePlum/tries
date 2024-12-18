c6e089cc2b4568a34a4b6b321e25655b121a8f5e
Deniz <denizalidurbin@gmail.com>
2024-12-18 22:29:15 +0100

    minor fix

--- testInsertdelete.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-./inserer 0 test/Shakespeare/allswell.txt
-./suppression 0 test/Shakespeare/allswell.txt
-
-./inserer 0 test/Shakespeare/macbeth.txt
-./suppression 0 test/Shakespeare/macbeth.txt
